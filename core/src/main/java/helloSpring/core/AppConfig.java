475d88d04173b0de7c6ec82ff8eaf0e4da8d4c24
Lee Jong Soo <jongsu2645@gmail.com>
2021-11-10 14:03:07 +0900

    Update new discount policy

--- core/src/main/java/helloSpring/core/AppConfig.java
+++ core/src/main/java/helloSpring/core/AppConfig.java
@@ -1,6 +1,9 @@
 package helloSpring.core;
 
+import helloSpring.core.discount.DiscountPolicy;
 import helloSpring.core.discount.FixDiscountPolicy;
+import helloSpring.core.discount.RateDiscountPolicy;
+import helloSpring.core.member.MemberRepository;
 import helloSpring.core.member.MemberService;
 import helloSpring.core.member.MemberServiceImpl;
 import helloSpring.core.member.MemoryMemberRepository;
@@ -10,10 +13,18 @@ import helloSpring.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 }

a7a9942df75a080811767e47aacf12af006d533c
Lee Jong Soo <jongsu2645@gmail.com>
2021-11-12 10:50:26 +0900

    Study Spring Bean Conatiner

--- core/src/main/java/helloSpring/core/MemberApp.java
+++ core/src/main/java/helloSpring/core/MemberApp.java
@@ -4,12 +4,18 @@ import helloSpring.core.member.Grade;
 import helloSpring.core.member.Member;
 import helloSpring.core.member.MemberService;
 import helloSpring.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 

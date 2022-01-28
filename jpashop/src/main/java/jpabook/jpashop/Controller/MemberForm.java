6c9edded1ec25d478c462d730baf9292d14e0744
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-01-28 17:09:04 +0900

    Fear: 회원 등록 및 조회

---
+++ jpashop/src/main/java/jpabook/jpashop/Controller/MemberForm.java
@@ -0,0 +1,20 @@
+package jpabook.jpashop.Controller;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Controller;
+
+import javax.validation.constraints.NotEmpty;
+
+@Getter @Setter
+@Controller
+public class MemberForm {
+
+    @NotEmpty(message = "회원 이름은 필수 입니다.")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+
+}

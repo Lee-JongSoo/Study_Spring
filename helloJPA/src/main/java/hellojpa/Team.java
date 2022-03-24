b654f74bc07c378a29759d2c7da1983ef3d92a97
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-03-24 18:51:35 +0900

    Study: Association

---
+++ helloJPA/src/main/java/hellojpa/Team.java
@@ -0,0 +1,41 @@
+package hellojpa;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+
+    @Id @GeneratedValue
+    @Column(name = "TEAM_ID")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member2> member2s = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public List<Member2> getMember2s() {
+        return member2s;
+    }
+
+    public void setMember2s(List<Member2> member2s) {
+        this.member2s = member2s;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
